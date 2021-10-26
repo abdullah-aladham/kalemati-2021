0b2625187ca6b145817c86cbc4bf4834de4efa63
Abdullah Aladham <abdullah.aladham@gmail.com>
2021-10-27 02:56:25 +0300

    security commit

--- Kalemati21/src/main/java/com/abdullah_aladham/Kalemati21/Security/ApplicationSecurityConfig.java
+++ Kalemati21/src/main/java/com/abdullah_aladham/Kalemati21/Security/ApplicationSecurityConfig.java
@@ -1,23 +1,30 @@
 package com.abdullah_aladham.Kalemati21.Security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+	private final PasswordEncoder passwordEncoder;
+	@Autowired
+	public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+		this.passwordEncoder=passwordEncoder;
+	}
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// TODO Auto-generated method stub
 		http
 			.authorizeRequests()
-			.antMatchers("/","index","/css/*","/js/*")
-			.permitAll()
 			.anyRequest()
 			.authenticated()
 			.and()
@@ -27,12 +34,24 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	@Bean
 	protected UserDetailsService userDetailsService() {
+		
 		// TODO Auto-generated method stub
-	User.builder()
-	.username("Sami")
-	.password("password")
-	.roles("STUDENT");
-	.
+	UserDetails TestUser =User.builder()
+	.username("Test")
+	.password(passwordEncoder.encode("password"))
+	.roles("CUSTOMER")
+	.build();
+	
+	UserDetails Admin_Abdullah =User.builder()
+	.username("Abdullah Aladham")
+	.password(passwordEncoder.encode("password"))
+	.roles("ADMIN")
+	.build();
+	return new InMemoryUserDetailsManager(
+			TestUser,
+			Admin_Abdullah
+			);
+	
 	}
 
 	
