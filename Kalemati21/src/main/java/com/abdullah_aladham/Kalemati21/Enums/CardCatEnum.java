642ab4f125575dfd7eddf0d7da6a678cf4aaa3fd
Abdullah Aladham <abdullah.aladham@gmail.com>
2022-03-23 22:53:48 +0200

    adding some features for sign up

---
+++ Kalemati21/src/main/java/com/abdullah_aladham/Kalemati21/Enums/CardCatEnum.java
@@ -0,0 +1,8 @@
+package com.abdullah_aladham.Kalemati21.Enums;
+/* this enum for card types*/
+public enum CardCatEnum {
+	Kitchen_and_food_tools,
+	Letter_Spelling,
+	
+
+}
---
+++ Kalemati21/src/main/java/com/abdullah_aladham/Kalemati21/Enums/GenderEnum.java
@@ -0,0 +1,6 @@
+package com.abdullah_aladham.Kalemati21.Enums;
+
+public enum GenderEnum {
+Male,
+Female,
+}
